302d9da649d6c8d682b8eb46597e610166438f5b
RudraNarayanMohanty <mrudra5675@gmail.com>
2023-03-23 23:01:38 +0530

    code checking

--- src/in/ashokit/App.java
+++ src/in/ashokit/App.java
@@ -2,7 +2,7 @@ package in.ashokit;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Santosh Modified");
+		System.out.println("Santosh Modified");//It's correct
 		PaymentService ps = new PaymentService(new SodexCardPayment());
 		ps.Payment(400.0);
 	}
